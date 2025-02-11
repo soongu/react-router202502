9f09ba10dffd1bb6dbb13476f2fafb5c50aebeff
soongu <naong526@naver.com>
2025-02-11 11:43:30 +0900

    # 3. 레이아웃 사용하기

--- src/routes/router-config.jsx
+++ src/routes/router-config.jsx
@@ -2,19 +2,26 @@ import { createBrowserRouter } from "react-router-dom";
 import IndexPage from "../pages/IndexPage";
 import BlogPage from "../pages/BlogPage";
 import AboutPage from "../pages/AboutPage";
+import RootLayout from "../layouts/RootLayout";
 
 // 라우터 설정
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <IndexPage />,
-  },
-  {
-    path: '/blog',
-    element: <BlogPage />,
-  },
-  {
-    path: '/about',
-    element: <AboutPage />,
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <IndexPage />,
+      },
+      {
+        path: 'blog',
+        element: <BlogPage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+    ],
   },
 ]);
