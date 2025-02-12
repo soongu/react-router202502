2dbbfca1cb00a8b0079f9b210427a4dd9f2e3c6f
soongu <naong526@naver.com>
2025-02-12 09:34:48 +0900

    # 5. 에러페이지 처리

--- src/routes/router-config.jsx
+++ src/routes/router-config.jsx
@@ -3,12 +3,14 @@ import IndexPage from "../pages/IndexPage";
 import BlogPage from "../pages/BlogPage";
 import AboutPage from "../pages/AboutPage";
 import RootLayout from "../layouts/RootLayout";
+import ErrorPage from "../pages/ErrorPage";
 
 // 라우터 설정
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
